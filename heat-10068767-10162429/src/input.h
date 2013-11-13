ff17d7c425f449c52982dd09d59e39010e87a1cd
Thomas Schoegje <T.M.Schoegje@students.uu.nl>
2013-11-13 12:06:23 +0100

    forgot to add folder

---
+++ heat-10068767-10162429/src/input.h
@@ -0,0 +1,36 @@
+#ifndef INPUT_H_H
+#define INPUT_H_H
+
+#include <stddef.h>
+
+/* input parameters for the program */
+struct parameters {
+    /* matrix size: N rows, M columns */
+    size_t N, M;
+
+    /* maximum number of iterations */
+    size_t maxiter;
+
+    /* number of iterations for the periodic report */
+    size_t period;
+
+    /* convergence threshold */
+    double threshold; 
+
+    /* initial temperature, in row-major order */
+    const double *tinit;
+
+    /* conductivity values for the cylinder, in row-major order */
+    const double *conductivity;
+
+    /* temperature range in input and output images */
+    double io_tmin;
+    double io_tmax;
+
+    /* number of threads */
+    size_t nthreads;
+};
+ 
+void read_parameters(struct parameters* p, int argc, char **argv);
+
+#endif
